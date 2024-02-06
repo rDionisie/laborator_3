a0d4d21443acd51d0ae6e7ab7a603dc34043ecce
Raileanu Dionisie <raileanu.dionisie@elev.cihcahul.md>
2024-02-06 10:32:53 +0200

    Rute

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/','/users');
+Route::get('/users',[UserController::class,'index'])->name('users.index');
+Route::get('/users/create',[UserController::class,'create'])->name('users.create');
+Route::post('/users',[UserController::class,'store'])-> name('users.store');
